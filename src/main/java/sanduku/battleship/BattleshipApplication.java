4a5fb596370369eb629b49c5456cbb0001a54421
ugolb <eugen.nagirnij@gmail.com>
2018-07-08 15:42:33 +0200

    Project structure is created

---
+++ src/main/java/sanduku/battleship/BattleshipApplication.java
@@ -0,0 +1,12 @@
+package sanduku.battleship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipApplication.class, args);
+	}
+}
